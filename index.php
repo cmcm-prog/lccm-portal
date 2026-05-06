b0ef20c40aaca5d56e506786808f7121402f0751
cmcm-prog <cmcasayuran@gmail.com>
2026-05-06 15:34:57 +0800

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ session_start();
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
 // 2. DATABASE CONNECTION
 $servername = "mysql.railway.internal"; 
